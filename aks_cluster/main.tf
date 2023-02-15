e2b775fca39ca898079157e9bd2066459971f120
IHEEB22 <iheb.hmida1000@gmail.com>
2023-02-15 12:22:19 +0100

    remove unused inputs in aks module

--- aks_cluster/main.tf
+++ aks_cluster/main.tf
@@ -6,8 +6,6 @@ module "aks" {
   agents_pool_name                  = var.aks_agents_pool_name
   location                          = var.aks_location
   agents_size                       = var.aks_agents_size
-  role_based_access_control_enabled = true
-  rbac_aad_azure_rbac_enabled       = true
-  rbac_aad_managed                  = true
-  rbac_aad                          = true
+  rbac_aad                          = var.rbac_aad
+  role_based_access_control_enabled = var.role_based_access_control_enabled
 }
